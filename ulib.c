3399cbb11a1d6efcc9b57cb661a02a914d571566
John <john@john-VirtualBox.(none)>
2016-03-22 21:12:40 -0400

    started on stage 2, added asm

--- ulib.c
+++ ulib.c
@@ -105,8 +105,27 @@ memmove(void *vdst, void *vsrc, int n)
   return vdst;
 } 
 
+
+void
+popVolatile(void){
+
+  __asm__(
+    "add $8, %esp;"
+    "pop %edx;"
+    "pop %ecx;"
+    "pop %eax;"
+    "ret");
+
+}
+
+
 int
 signal(int signum, void* handler){
+
+  //This should get us the address of the function popVolatile??
+  void popVolatile();
+  void (*popVolatile_fun)() = &popVolatile;
+
   handler = (sighandler_t)handler;
   register_signal_handler(signum, handler);
   return 0;
